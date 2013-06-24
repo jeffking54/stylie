0c979e7f0e43207f4ef2a72f616f054b0c94ba13
Jeremy Kahn <jeremyckahn@gmail.com>
2013-06-23 17:02:25 -0700

    deleted: src/ui/keyframe.js

--- src/ui/keyframe.js
+++
@@ -1,9 +0,0 @@
-define(['src/app'], function (app) {
-
-  // TODO: Delete this file.
-  return Backbone.View.extend({
-    'initialize': function (opts) {
-      _.extend(this, opts);
-    }
-  });
-});
