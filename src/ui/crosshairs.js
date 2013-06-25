0cb502ebeff7c5e57b6094982bcf2ba9c3ce8297
Jeremy Kahn <jeremyckahn@gmail.com>
2013-06-24 21:16:21 -0700

    Removes from and to classes from crosshairs.

--- src/ui/crosshairs.js
+++ src/ui/crosshairs.js
@@ -1,21 +1,13 @@
 define(['src/app', 'src/ui/crosshair'], function (app, CrosshairView) {
 
   var CROSSHAIR_TEMPLATE = [
-    '<div class="crosshair {{extraClass}}" data-pos="{{position}}" data-millisecond="{{ms}}">'
+    '<div class="crosshair">'
       ,'<div class="dashmark horiz"></div>'
       ,'<div class="dashmark vert"></div>'
       ,'<div class="rotation-arm">'
         ,'<div class="rotation-handle">'
       ,'</div>'
     ,'</div>'].join('');
-
-  function generateCrosshairHtml (extraClass, position, millisecond) {
-    return Mustache.render(CROSSHAIR_TEMPLATE, {
-      'extraClass': extraClass
-      ,'position': position
-      ,'millisecond': millisecond
-    });
-  }
 
   return Backbone.View.extend({
 
@@ -26,11 +18,7 @@ define(['src/app', 'src/ui/crosshair'], function (app, CrosshairView) {
 
     ,'addCrosshairView': function (model) {
       var keyframeCount = app.collection.actors.getCurrent().getLength();
-
-      var $el = keyframeCount % 2
-          ? $(generateCrosshairHtml('from', 'from', model.get('millisecond')))
-          : $(generateCrosshairHtml('to', 'to', model.get('millisecond')));
-
+      var $el = $(Mustache.render(CROSSHAIR_TEMPLATE));
       this.$el.append($el);
 
       this.crosshairViews[model.cid] = new CrosshairView({
